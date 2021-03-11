0836e8a1af254c5bd0941c470f54ae3f72f1a4e8
AbbasRaziq <78424813+AbbasRaziq@users.noreply.github.com>
2021-03-11 15:53:35 +0000

    Update InputReader.java

--- app05-stock/InputReader.java
+++ app05-stock/InputReader.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
  * 
  * @author     Michael Kölling and David J. Barnes
  * @version    0.1 (2016.02.29)
+ * 
+ * Modified by Derek Peacock 13/12/2020
  */
 public class InputReader
 {
@@ -26,11 +28,49 @@ public class InputReader
      *
      * @return  A String typed by the user.
      */
-    public String getInput()
+    public String getString(String prompt)
     {
-        System.out.print("> ");         // print prompt
-        String inputLine = reader.nextLine();
+        String inputLine = null;
+        boolean isValid = false;
+        
+        while(!isValid)
+        {
+            System.out.print(prompt);         // print prompt
+            inputLine = reader.nextLine();
+            
+            if(!inputLine.isEmpty())
+                isValid = true;
+            else 
+                System.out.println("Your entry is blank!");
+        }
 
         return inputLine;
+    }
+    
+    /**
+     * Read a the next int from standard imput (the text terminal),
+     * and return it as an interger.
+     * 
+     * @return Integer typed by user.
+     */
+    public int getInt(String prompt)
+    {
+        int number = 0;
+        boolean isValid = false;
+        
+        while(!isValid)
+        {
+            System.out.println(prompt);         // print prompt
+            number = reader.nextInt();
+            
+            if(number > 0)
+            {
+                isValid = true;
+            }
+            else 
+                System.out.println("Your value is less than zero!");
+        }
+
+        return number;
     }
 }
