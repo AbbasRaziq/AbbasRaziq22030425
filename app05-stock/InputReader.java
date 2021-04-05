9ce4b7cc5a8d5164d02aa8743875105bc563306a
AbbasRaziq <78424813+AbbasRaziq@users.noreply.github.com>
2021-04-05 21:57:50 +0100

    Update InputReader.java

--- app05-stock/InputReader.java
+++ app05-stock/InputReader.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
  * InputReader reads typed text input from the standard text terminal. 
  * The text typed by a user is returned.
  * 
- * @author     Michael Kölling and David J. Barnes
- * @version    0.1 (2016.02.29)
+ * @author Abbas Raziq
+ * @version 01/04/2021
  * 
- * Modified by Derek Peacock 13/12/2020
  */
 public class InputReader
 {
