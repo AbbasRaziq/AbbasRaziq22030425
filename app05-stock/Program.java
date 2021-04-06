2db6714100355acc23f2e73e3350a1c4d040608b
AbbasRaziq <78424813+AbbasRaziq@users.noreply.github.com>
2021-04-06 11:13:04 +0100

    Update Program.java

--- app05-stock/Program.java
+++ app05-stock/Program.java
@@ -5,7 +5,7 @@ import java.lang.InterruptedException;
  * Write a description of class Program here.
  *
  * @author Abbas Raziq
- * @version 01/04/2021
+ * @version 02/04/2021
  */
 public class Program
 {
